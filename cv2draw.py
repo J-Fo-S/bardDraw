9ca9f6ce1649dc394b32d9d12b3bd4a3297ed163
J-Fo-S <sherm_jonathan@hotmail.com>
2024-03-18 10:21:34 +0800

    new modes

--- cv2draw.py
+++ cv2draw.py
@@ -3,6 +3,8 @@ import cv2
 import mediapipe as mp
 import numpy as np
 import sys
+import time
+from quickdraw import QuickDrawDataGroup
 from collections import deque
 
 class CV2Draw:
@@ -16,21 +18,21 @@ class CV2Draw:
                                         min_tracking_confidence=0.5)
         self.mpDraw = mp.solutions.drawing_utils
         self.cap = cv2.VideoCapture(0)
+        self.canvas = np.ones((480, 640, 3), np.uint8) * 255
     
     def capture(self, query, ticket, im_request, exit_event, args):
         # For webcam input:
-        canvas = np.ones((480, 640, 3), np.uint8) * 255
         last_point = None
         smooth = args.smooth
         mode_len = args.mode_len
-        num_fingers_r_list =deque([], mode_len)
+        num_fingers_r_list = deque([], mode_len)
         num_fingers_l_list = deque([], mode_len)
         while self.cap.isOpened():
             if exit_event.is_set():
                 print("_kill cv2")
                 sys.exit()
             _, frame = self.cap.read()
-            frame = cv2.resize(frame, (canvas.shape[0], canvas.shape[1]))
+            frame = cv2.resize(frame, (self.canvas.shape[0], self.canvas.shape[1]))
             x, y, c = frame.shape
             frame = cv2.flip(frame, 1)
             framergb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
@@ -65,7 +67,7 @@ class CV2Draw:
                             gesture = 'heavy line'
                         elif num_fingers_r_ave == 3:
                             gesture = 'erase'
-                    if hand_lr == 0 and args.mode == 'sign':
+                    if hand_lr == 0 and args.user_mode == 'sign':
                         if len(landmarks) > 0:
                             if landmarks[8][1] < landmarks[6][1]:
                                 num_fingers_l += 1
@@ -87,20 +89,20 @@ class CV2Draw:
                     current_point = (np.array(landmarks[8]) * (1-smooth) + np.array(last_point) * smooth).astype(int)
                     if gesture == 'light line':
                         if last_point is not None:
-                            cv2.line(canvas, last_point, current_point, (0, 0, 0), 2)
+                            cv2.line(self.canvas, last_point, current_point, (0, 0, 0), 2)
                         last_point = current_point
                     elif gesture == 'heavy line':
                         if last_point is not None:
-                            cv2.line(canvas, last_point, current_point, (0, 0, 0), 8)
+                            cv2.line(self.canvas, last_point, current_point, (0, 0, 0), 8)
                         last_point = current_point
                     elif gesture == 'erase':
-                        cv2.rectangle(canvas, (0, 0), (canvas.shape[1], canvas.shape[0]), (255, 255, 255), -1)
+                        cv2.rectangle(self.canvas, (0, 0), (self.canvas.shape[1], self.canvas.shape[0]), (255, 255, 255), -1)
                         last_point = None
                     else:
                         if last_point is not None:
                             last_point = landmarks[8]
             if (answer == 'Guess the drawing' or im_request.is_set()) and ticket.is_set():
-                canvas_jpg = cv2.resize(canvas, (64, 64))
+                canvas_jpg = cv2.resize(self.canvas, (64, 64))
                 _, canvas_jpg = cv2.imencode('.jpg', canvas_jpg)
                 print("_using ticket for image")
                 ticket.clear()
@@ -113,7 +115,7 @@ class CV2Draw:
                 ticket.clear()
                 print("_sent query")
                 query.put(answer)
-            cv2.imshow("Board", canvas)
+            cv2.imshow("Board", self.canvas)
             cv2.putText(frame, gesture, (10, 50), cv2.FONT_HERSHEY_SIMPLEX, 
                         1, (0,0,255), 2, cv2.LINE_AA)
             cv2.putText(frame, answer, (10, 100), cv2.FONT_HERSHEY_SIMPLEX, 
@@ -123,3 +125,58 @@ class CV2Draw:
                 break
         self.cap.release()
         cv2.destroyAllWindows()
+
+    def quickdoodle(self, doodle, exit_event):
+        while True:
+            if exit_event.is_set():
+                print("_kill quickdraw")
+                sys.exit()
+            if not doodle.empty():
+                print("_getting doodle")
+                dood = str(doodle.get())
+                if doodle.qsize() >= 1:
+                    qd = QuickDrawDataGroup(dood, max_drawings=4, recognized=True)
+                else:
+                    qd = QuickDrawDataGroup(dood, max_drawings=100, recognized=True)
+                for _, draw in enumerate(qd.drawings):
+                    if exit_event.is_set():
+                        print("_kill quickdraw")
+                        sys.exit()
+                    if not doodle.empty():
+                            break
+                    minx = 10000000
+                    maxx = 0
+                    miny = 10000000
+                    maxy = 0
+                    for stroke in draw.strokes:
+                        for coordinate in range(len(stroke)-1):
+                            x1 = stroke[coordinate][0]
+                            #print(f'x1{x1}')
+                            y1 = stroke[coordinate][1] + 30
+                            x2 = stroke[coordinate+1][0]
+                            #print(f'x2{x2}')
+                            y2 = stroke[coordinate+1][1] + 30
+                            cv2.line(self.canvas, (x1, y1), (x2, y2), (0, 0, 255), 4)
+                            if minx < x1:
+                                minx = x1
+                            if minx < x2:
+                                minx = x2
+                            if maxx > x1:
+                                maxx = x1
+                            if maxx > x2:
+                                maxx = x2
+                            if miny < y1:
+                                miny = y1
+                            if miny < y2:
+                                miny = y2
+                            if maxy > y1:
+                                maxy = y1
+                            if maxy > y2:
+                                maxy = y2
+                            time.sleep(0.1)
+                    cv2.putText(self.canvas, dood, (maxx, maxy+20), cv2.FONT_HERSHEY_SIMPLEX, 
+                        1, (0,0,255), 2, cv2.LINE_AA)
+                    time.sleep(4)
+                    cv2.rectangle(self.canvas, (minx, miny), (maxy, maxy), (255, 255, 255), -1)
+            time.sleep(0.1)
+            pass
--- main.py
+++ main.py
@@ -4,6 +4,7 @@
 # add funcs here: https://colab.research.google.com/drive/1zzzlTIh0kt2MdjLzvXRby1rWbHzmog8t?usp=sharing#scrollTo=gU9J5HUp7PfW
 import requests
 import sys
+import os
 import threading, time
 import queue
 import signal
@@ -13,15 +14,30 @@ import time
 import keyboard
 import speech_recognition as sr
 import argparse
+import csv
+from collections import deque
 from bardapi import BardCookies
 from cv2draw import CV2Draw
-from quickdrawim import quickdoodle
 from t2s import T2S
-import os
+
 
 # for exiting the threads
 def signal_handler(signum, frame):
     exit_event.set()
+
+def reply_log(single_reply, self_refine, critic_refine):
+    # setup to timestamp whatever scores to csv file
+    with open('session.csv', 'w', newline='') as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow(["single reply", "self-refined reply", "critic-refined reply"])
+        if self_refine:
+            writer.writerow([single_reply.popleft(), self_refine.popleft(), "N/A"])
+        elif critic_refine:
+            writer.writerow([single_reply.popleft(), "N/A", critic_refine.popleft()])
+        elif single_reply:
+                writer.writerow([single_reply.popleft(), "N/A", "N/A"])
+        print("_session saved to csv")
+        sys.exit()
 
 def user_input(query, ticket, exit_event, args):
     while True:
@@ -33,7 +49,7 @@ def user_input(query, ticket, exit_event, args):
         time.sleep(0.1)
         if ticket.is_set():
             try:
-                if args.mode == 'speech' :
+                if args.user_mode == 'speech' :
                     t = []
                     with mic as source:
                         r.adjust_for_ambient_noise(source, duration=0.5)
@@ -63,7 +79,7 @@ def user_input(query, ticket, exit_event, args):
                             except sr.UnknownValueError:
                                 print("_No User Voice detected OR unintelligible noises detected OR the recognized audio cannot be matched to text !!!")
                                 pass
-                elif args.mode == 'keyboard':
+                elif args.user_mode == 'keyboard':
                     q = input()
                     if not q == '':
                         if 'guess' in q.split():
@@ -82,9 +98,10 @@ def user_input(query, ticket, exit_event, args):
     
 if __name__ == '__main__':
     #suppress errors/warnings
-    sys.stderr = open(os.devnull, "w")
+    #sys.stderr = open(os.devnull, "w")
     parser = argparse.ArgumentParser()
-    parser.add_argument('--mode', type=str, default='keyboard', choices=['keyboard', 'sign', 'speech'])
+    parser.add_argument('--user_mode', type=str, default='keyboard', choices=['keyboard', 'sign', 'speech'])
+    parser.add_argument('--ref_mode', type=str, default='text_ref', choices=['text_ref', 'self_ref', 'critic_ref'])
     parser.add_argument('--mode_len', type=int, default=20, help='number of frames to evaluate gesture classification')
     parser.add_argument('--smooth', type=float, default=0.9, help='val between 0 and 1 to smooth drawing')
     args = parser.parse_args()
@@ -130,6 +147,8 @@ if __name__ == '__main__':
     session.cookies.set("NID",nid_value)
     session.headers = SESSION_HEADERS
     bard = BardCookies(session=session, cookie_dict=cookie_dict)
+    if args.ref_mode == 'critic_ref':
+        bard_critic = BardCookies(session=session, cookie_dict=cookie_dict)
     # set up speech/mic input
     r = sr.Recognizer()
     mic = sr.Microphone()
@@ -146,6 +165,10 @@ if __name__ == '__main__':
     doodle = queue.Queue(maxsize=1)
     # queue for generating speech
     speech = queue.Queue()
+    # deque for saving end results
+    single_reply = deque()
+    self_refine = deque()
+    critic_refine = deque()
     # event for polling for queries
     ticket = threading.Event()
     # event for collecting cv2 image
@@ -161,19 +184,20 @@ if __name__ == '__main__':
     # threads for running concurrent programs
     cv_draw = threading.Thread(target=cv2draw.capture, args=(query, ticket, im_request, exit_event, args))
     cv_draw.start()
-    draw_doodle = threading.Thread(target=quickdoodle, args=(doodle, exit_event))
+    draw_doodle = threading.Thread(target=cv2draw.quickdoodle, args=(doodle, exit_event))
     draw_doodle.start()
     t2s = T2S()
-    print('_you are in {0} mode'.format(args.mode))
+    print('_you are in {0} user_mode'.format(args.user_mode))
     computer_speech = threading.Thread(target=t2s.t2s_thread, args=(speech, exit_t2s_event))
     computer_speech.start()
+    session_csv = threading.Thread(target=reply_log, args=(single_reply, self_refine, critic_refine))
     # start off polling
     ticket.set()
     # collect text guidence files
     txt_guide = []
     txt_files = ['user_info.txt', 'doodle_list.txt', 'doodle_guess_instruction.txt', 'doodle_guess_terms.txt',
                   'doodle_guess_again_terms.txt', 'doodle_guess_again_instruction.txt', 'doodle_extension_terms.txt', 
-                  'doodle_extension_instruction.txt', 'doodle_recap_instruction.txt']
+                  'doodle_extension_instruction.txt', 'doodle_recap_instruction.txt', 'doodle_refine_instruction.txt']
     speech.put(['ask away'])
     print('ask away!')
     for txt in txt_files:
@@ -220,8 +244,18 @@ if __name__ == '__main__':
                                 doodle.put(item, block=False)
                             except queue.Full:
                                 pass
+                print("reply: " + reply)
+                # for .csv file
+                single_reply.append(reply)
+                if args.ref_mode == 'self_ref':
+                    reply = bard.get_answer(txt_guide[9] + reply)['content']
+                    print("reply self refined: " + reply)
+                    self_refine.append(reply)
+                elif args.ref_mode == 'critic_ref':
+                    reply = bard_critic.get_answer(txt_guide[9] + reply)['content']
+                    print("reply critic refined: " + reply)
+                    critic_refine.append(reply)
                 speech.put(reply)
-                print(reply)
                 print("_done")
                 ticket.set()
                 print("_issuing new ticket")
@@ -232,8 +266,18 @@ if __name__ == '__main__':
             speech.put(["let's recap"])
             print("let's recap")
             reply = bard.get_answer(txt_guide[8])['content']
+            print("reply: " + reply)
+            single_reply.append(reply)
+            if args.ref_mode == 'self_ref':
+                reply = bard.get_answer(txt_guide[9] + reply)['content']
+                print("reply self refined: " + reply)
+                self_refine.append(reply)
+            elif args.ref_mode == 'critic_ref':
+                reply = bard_critic.get_answer(txt_guide[9] + reply)['content']
+                print("reply critic refined: " + reply)
+                critic_refine.append(reply)
+            session_csv.start()
             speech.put(reply) 
-            print(reply)
             exit_t2s_event.set()
             sys.exit()
         pass
